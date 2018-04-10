ea6d1cd9661c63589770cb8bf95ac951ee883060
Yan Wang <yan.wang@agon-solutions.de>
2018-04-10 13:15:05 +0200

    update

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 #!flask/bin/python
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template
 from chatterbot.trainers import ListTrainer # method to train the chatbot
 from chatterbot import ChatBot # import the chatbot
 import urllib.parse
 import os
+
 bot = ChatBot('TestBot') # create the chatbot
 bot.set_trainer(ListTrainer)# set the trainer test
 
+# get current python file path
 file_path = os.path.abspath(__file__)
+# get directory files full path
 files_path = os.path.join(os.path.dirname(file_path), 'files')
 
-
+# walk through files directory to find out conversation file
 for root, dirs, files in os.walk(files_path, topdown=False):
     for name in files:
         print('name in files: '+os.path.join(root, name))
@@ -20,17 +23,16 @@ for root, dirs, files in os.walk(files_path, topdown=False):
 
 app = Flask(__name__)
 
-@app.route('/chatbot')
-def chatbot():
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-    return "welcome to chatbot parent "
-
-@app.route('/chatbot/conversations', methods=['GET'])
+@app.route('/get', methods=['GET'])
 def get_reply():
-    question = request.args.get('question')
+    question = request.args.get('msg')
     question=urllib.parse.unquote(question)
     response = bot.get_response(question)
-    replystr='you: '+question + '<br>' + 'bot: '+str(response)
+    replystr=str(response)
     return replystr
 
 if __name__ == '__main__':
